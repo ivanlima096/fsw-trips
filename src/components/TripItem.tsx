3171cedff1daf7061be488b17803e16726bc682a
ivanlima096 <ivanlima096@gmail.com>
2023-09-22 12:56:23 -0300

    feat: add tripItem Component and built recommendedTrips

---
+++ src/components/TripItem.tsx
@@ -0,0 +1,27 @@
+import { Trip } from "@prisma/client"
+import Image from "next/image"
+import ReactCountryFlag from "react-country-flag"
+
+interface TripItemProps {
+  trip: Trip
+}
+
+export default function TripItem({ trip }: TripItemProps) {
+
+  return (
+    <div className="flex flex-col">
+      <div className="relative w-[280px] h-[280px]">
+        <Image src={trip.coverImage} className="rounded-lg shadow-md object-cover" alt={trip.name} fill />
+      </div>
+      <h3 className="text-primaryDark font-medium text-sm mt-2">{trip.name}</h3>
+      <div className="flex items-center gap-1 my-1">
+        <ReactCountryFlag countryCode={trip.contryCode} svg />
+        <p className="text-xs text-grayPrimary">{trip.location}</p>
+      </div>
+      <p className="text-xs text-grayPrimary">
+        <span className="text-primary font-medium ">R$ {trip.pricePerDay.toString()}</span> por dia
+      </p>
+
+    </div>
+  )
+}
